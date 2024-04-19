6fa32e4099ae079873bb9f7019c0b6dc305a4b16
Dmitry Akatov <dmitry.akatov@protonmail.com>
2024-04-19 20:07:31 +0300

    tests and static tools

--- devenv.nix
+++ devenv.nix
@@ -12,17 +12,17 @@
     stack
     universal-ctags
     haskellPackages.hoogle
+    haskellPackages.ghcid
+    haskellPackages.ormolu
+    haskellPackages.hlint
+    haskellPackages.implicit-hie
+    haskellPackages.retrie
     haskellPackages.hasktags
     haskellPackages.haskdogs
     haskell.compiler.ghc945
     haskell.packages.ghc945.haskell-language-server
     haskell.packages.ghc945.hlint
   ];
-
-  scripts.run-tests.exec = ''
-    hlint src --report
-    stack test
-  '';
 
   scripts.wake.exec = ''
     haskdogs --deps-dir ".haskdogs" --use-stack ON
@@ -31,6 +31,11 @@
   enterShell = ''
     echo "Hello fellow hacker"
     ghc --version
+  '';
+
+  enterTest = ''
+    hlint src --report
+    stack test
   '';
 
 }
