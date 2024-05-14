6877d7ca9382899d04d6c544f1f34adb5b20db46
Dmitry Akatov <dmitry.akatov@protonmail.com>
2024-05-14 12:40:40 +0300

    initial commit

--- app/Main.hs
+++ app/Main.hs
@@ -20,16 +20,12 @@ parse [] = do
   repl
   exitSuccess
 
-parse ["--help"] = do
-  version
-  usage
-  exitSuccess
+-- parse ["--help"] = do
+--   version
+--   usage
+--   exitSuccess
 
-parse ["--version"] = do
-  version
-  exitSuccess
-
-parse ("--context":filename:_) = do
+parse (filename:_) = do
   content <- Text.pack . BSChar8.unpack <$> BS.readFile filename
 
   let context = mempty :: OrgContext
@@ -39,15 +35,15 @@ parse ("--context":filename:_) = do
   runRepl "mydatabase.sqlite" 10 context' Org.parse
   exitSuccess
 
-parse (x:xs) = do
-  putStrLn ("Unknown argument skipped: " ++ x)
-  parse xs
+-- parse (x:xs) = do
+--   putStrLn ("Unknown argument skipped: " ++ x)
+--   parse xs
 
 repl :: IO ()
 repl = runRepl "mydatabase.sqlite" 10 (mempty::OrgContext) Org.parse
 
 usage :: IO ()
-usage   = putStrLn "Usage: glance [--repl | --help | --version | --file [filename] ]"
+usage   = putStrLn "Usage: glance [filename]"
 
 version :: IO ()
 version = putStrLn "Haskell glance 0.1.0.0"
